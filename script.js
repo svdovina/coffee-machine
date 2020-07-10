794d3f7e6124174c389023bd33d4c3d085bae70e
svdovina <svetlaiy@gmail.com>
2020-07-10 17:51:33 +0300

    Добавление купюр и их поедание купюроприемником

--- script.js
+++ script.js
@@ -87,3 +87,123 @@ function changeDisplayText(text) {
   let displayText = document.querySelector(".display span");
   displayText.innerHTML = text;
 }
+
+//----------------Drag'n'Drop----------------
+let money = document.querySelectorAll(".money img"); //получаем массив всех купюр
+//два варианта для перебора всего массива с for
+//1
+//for (let i = 0; i < money.length; i++) {
+//  money[i].onclick = takeMoney; //изменения внутри массива будут
+//}
+
+//2
+//В функцию, которая присвоена событию, передается this, который возвращает элемент, на котором это событие совершено
+for (let bill of money) {
+  bill.onmousedown = takeMoney; //изменений в массиве не будет
+}
+//В функцию, которая присвоена событию, первым параметром передается объект события - event, 
+
+function takeMoney(event) {
+  event.preventDefault();
+//  console.log(this);
+//  console.log(event);
+//  console.log([event.target, event.clientX, event.clientY]);
+  let bill = this;
+  
+//  console.log( bill.style.height ); //""
+//  console.log( bill.style.width ); //""
+//  console.log( bill.getBoundingClientRect() );
+  
+  let billCoords = bill.getBoundingClientRect(); //Получение объекта, которые описывает положение элемента
+  
+  let billHeight = billCoords.height;
+  let billWidth = billCoords.width;
+  
+  bill.style.position = "absolute";
+  if (!bill.style.transform) { //bill.style.transform == ""
+    bill.style.top = (event.clientY - billHeight/2 + "px");
+    bill.style.left = event.clientX - billWidth/2 + "px";
+    bill.style.transform = "rotate(90deg)";
+  } else {
+    bill.style.top = (event.clientY - billWidth/2 + "px");
+    bill.style.left = (event.clientX - billHeight/2 + "px");
+  }
+  
+  bill.style.transition = "transform .3s";
+  
+  window.onmousemove = function (event) {
+    let billCoords = bill.getBoundingClientRect(); 
+    let billHeight = billCoords.height;
+    let billWidth = billCoords.width;
+    bill.style.top = (event.clientY - billWidth/2 + "px");
+    bill.style.left = (event.clientX - billHeight/2 + "px");
+  };
+  
+  bill.onmouseup = function() {
+    window.onmousemove = null;
+    if ( inAtm(bill) ) {
+    console.log( bill.getAttribute("data-cost") );
+    console.log( bill.dataset.cost );
+    balance.value = +balance.value + +bill.dataset.cost;
+    bill.remove(); //Удаляем элемент
+    }
+  };
+}
+
+function inAtm(bill) {
+  let atm = document.querySelector(".atm img");
+  
+  let atmCoords = atm.getBoundingClientRect();
+  let atmLeftX = atmCoords.x;
+  let atmRightX = atmCoords.x + atmCoords.width;
+  let atmTopY = atmCoords.y;
+  let atmBottomY = atmCoords.y + atmCoords.height/3;
+  
+  let billCoords = bill.getBoundingClientRect();
+  let billLeftX = billCoords.x;
+  let billRightX = billCoords.x + billCoords.width;
+  let billY = billCoords.y;
+  if(
+        billLeftX > atmLeftX
+    &&  billRightX < atmRightX
+    &&  billY > atmTopY
+    &&  billY < atmBottomY
+    ) {
+    return true;
+  } else {
+    return false;
+  }
+  
+//  return {
+//    atm: [atmLeftX, atmRightX, atmTopY, atmBottomY],
+//    bill: [billLeftX, billRightX, billY],
+//  };
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
